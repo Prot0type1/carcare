adf3793c43327d1f5c2e7db765a104b17171a158
Mario Barraza <mbarraz91@gmail.com>
2016-12-12 17:47:08 -0800

    learning to merge

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/profile', function(req,res,next){
+      res.render('profile', { title: 'Profile'});
+});
+
+router.get('/register', function(req,res,next){
+      res.render('register', { title: 'Registration'});
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CarCare' });
 });
 
 
 router.get('/users', function(req, res, next) {
   res.render('users', { title: 'Home' });
 });
+
 
 module.exports = router;
